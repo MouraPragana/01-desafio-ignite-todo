8220aea906fdd7bee17e9a3d1e72f46cef1506fa
Mateus Pragana <mateuspragana@gmail.com>
2022-08-17 22:09:10 -0300

    Finalizando NoTodoList componente e sua estilização

--- src/Components/NoTodoList.tsx
+++ src/Components/NoTodoList.tsx
@@ -1,8 +1,11 @@
+import clipboard from "../Assets/clipboard.svg"
+
 import style from "./NoTodoList.module.css"
 
 export function NoTodoList() {
     return (
         <div className={style.noTodoContent}>
+            <img src={clipboard} />
             <strong>Você ainda não tem terefas cadastradas</strong>
             <p>Crie tarefas e organize seus itens a fazer</p>
         </div>
